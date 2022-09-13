735e229f0506c4afbb52c68d76a2c52123b4e534
email <tarunmehto71@gmail.com>
2022-09-13 23:09:36 +0530

    prime problem

---
+++ prime_no.js
@@ -0,0 +1,17 @@
+let num = 13;
+let prime = true;
+    
+    for(let x=2; x<num; x++){
+        
+        if(num % x == 0){
+            
+            prime = true;
+        }
+    }
+    
+    if(prime == true){
+        console.log("Yes");
+        
+    }else{
+        console.log("No");
+    }
