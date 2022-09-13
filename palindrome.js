5c51fde8dffb23751edb79b94c52fc16e3530cc2
Raushan <raushansharma7462@gmail.com>
2022-09-13 23:33:19 +0530

    palingrome or prime NO.

--- palindrome.js
+++ palindrome.js
@@ -1,19 +1,21 @@
-let str = "nurpul";
-let N=6;
+
+let str = 'nrupul';
+let N = 6;
 
 let store1 = '';
-    let store2 = '';
+let store2 = '';
+
+for(let x=0; x<str.length; x++){
+    store1 = store1 + str[x]
+}
+for(let y=N-1; y>=0; y--){
+    store2 = store2 + str[y]
+}
+
+if(store1 == store2){
+    console.log('Yes');
     
-    for(let x=0; x<str.length; x++){
-        store1 = store1 + str[x]
-    }
-    for(let y=N-1; y>=0; y--){
-        store2 = store2 + str[x]
-    }
-    
-    if(store1 == store2){
-        console.log('no');
-        
-    }else{
-        console.log('No');
-    }
+}else{
+    console.log('No');
+}
+console.log(str)
